b1bd4c50fce4286d82b5e3f75df125d580b9aac6
Rishabh Animesh <ra523@cornell.edu>
2013-03-21 00:05:04 -0400

    Building the mentions matrix

--- src/XMLParsing.java
+++ src/XMLParsing.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,15 +13,35 @@ import com.meng.parsing.xml.Opinions;
 public class XMLParsing {
 
 	static HashMap<String, Integer> users = new HashMap<String, Integer>();
+	static HashMap<String, Integer> userToID = new HashMap<String, Integer>();
+	static HashMap<Integer, String> IDToUser = new HashMap<Integer, String>();
+	static HashMap<String, ArrayList<String>> associations = new HashMap<String, ArrayList<String>>();
+	static int userIDs = 1;
 
 	XMLParsing() {
 
 	}
 
+	public static void countUsersOccuringMoreThanOnce() {
+		int count = 0;
+		for (String userID : users.keySet()) {
+			if (users.get(userID) > 1) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
 	public static void parseOpinion(Opinion opinion) {
 		String opinionHolderID = opinion.getOpId();
 		if (!users.containsKey(opinionHolderID)) {
-			users.put(opinionHolderID, 1);
+			userToID.put(opinionHolderID, userIDs);
+			IDToUser.put(userIDs, opinionHolderID);
+			userIDs = userIDs + 1;
+			users.put(opinionHolderID, 1);			
+			ArrayList <String> referencees = new ArrayList<String>();
+			associations.put(opinionHolderID, referencees);
+			
 		} else {
 			users.put(opinionHolderID, users.get(opinionHolderID) + 1);
 		}
@@ -28,10 +49,18 @@ public class XMLParsing {
 		List<String> referencees = references.getOhRef();
 		for (String referenceeID : referencees) {
 			if (!users.containsKey(referenceeID)) {
-				users.put(referenceeID, 1);
+				users.put(referenceeID, 1);				
+				userToID.put(referenceeID, userIDs);
+				IDToUser.put(userIDs, referenceeID);
+				userIDs = userIDs + 1;
+			
 			} else {
 				users.put(referenceeID, users.get(referenceeID) + 1);
 			}
+			
+			ArrayList <String> referenceesList = associations.get(opinionHolderID);
+			referenceesList.add(referenceeID);
+			associations.put(opinionHolderID, referenceesList);
 		}
 	}
 
@@ -39,7 +68,8 @@ public class XMLParsing {
 		try {
 			JAXBContext jc = JAXBContext.newInstance("com.meng.parsing.xml");
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
-			Opinions ops = (Opinions) unmarshaller.unmarshal(new File(filename));
+			Opinions ops = (Opinions) unmarshaller
+					.unmarshal(new File(filename));
 			List<Opinion> opn = ops.getOpinion();
 			for (int i = 0; i < opn.size(); i++) {
 				parseOpinion(opn.get(i));
@@ -49,22 +79,24 @@ public class XMLParsing {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void readFile() {
 		String filename = "";
-		
+
 		filename = "1.xml";
 		getOpinions(filename);
-		
+
 		filename = "2.xml";
 		getOpinions(filename);
 	}
-	
+
 	public static void test() {
 		try {
 			JAXBContext jc = JAXBContext.newInstance("com.meng.parsing.xml");
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
-			Opinions ops = (Opinions) unmarshaller.unmarshal(new File("1.xml"));
+			unmarshaller.setProperty("jaxb.encoding", "ASCII");
+			Opinions ops = (Opinions) unmarshaller.unmarshal(new File(
+					"andersonCooper1.xml"));
 			List<Opinion> opn = ops.getOpinion();
 			for (int i = 0; i < opn.size(); i++) {
 				parseOpinion(opn.get(i));
@@ -78,7 +110,8 @@ public class XMLParsing {
 	}
 
 	public static void main(String[] args) {
-		readFile();
+		// readFile();
+		test();
 		System.out.println(users.size());
 	}
 }
---
+++ src/mentionMatrix/BuildMentionsMatrix.java
@@ -0,0 +1,44 @@
+package mentionMatrix;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BuildMentionsMatrix {
+	HashMap<String, Integer> userToID;
+	HashMap<Integer, String> IDToUser;
+	int SIZE;
+	double mentions[][];
+
+	BuildMentionsMatrix(HashMap<String, ArrayList<String>> associations,
+			HashMap<String, Integer> userToID, HashMap<Integer, String> IDToUser) {
+
+		this.userToID = userToID;
+		this.IDToUser = IDToUser;
+		this.SIZE = associations.size();
+
+		constructLinkMatrix(associations);
+	}
+
+	public void constructLinkMatrix(
+			HashMap<String, ArrayList<String>> associations) {
+
+		initializeLinkMatrix();
+
+		for (String userID : associations.keySet()) {
+			ArrayList<String> mentionedIDs = associations.get(userID);
+			for (String mentionedID : mentionedIDs) {
+				mentions[userToID.get(userID)][userToID.get(mentionedID)] = mentions[userToID
+						.get(userID)][userToID.get(mentionedID)] + 1.0;
+			}
+		}
+
+	}
+
+	public void initializeLinkMatrix() {
+		for (int i = 0; i < this.SIZE; i++) {
+			for (int j = 0; j < this.SIZE; j++) {
+				mentions[i][j] = 0.0;
+			}
+		}
+	}
+}
